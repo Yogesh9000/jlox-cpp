126ee3a29e9e6ecf2bf260b4a4d5fb42976ca275
Yogesh Rathod <yogesh952003@gmail.com>
2024-12-02 21:51:47 +0530

    Add variable Error::hadRuntimeError for runtime error

--- src/common.hpp
+++ src/common.hpp
@@ -9,11 +9,14 @@ constexpr int EX_USAGE = 64;
 constexpr int EX_DATAERR = 65;
 // An input file did not exist or was not readable.
 constexpr int EX_NOINPUT = 66;
+// Runtime Error
+constexpr int EX_RUNTIME = 70;
 
 namespace Error
 {
   // hadError is set whenever there is any error during compilation process
   inline bool hadError = false;
+  inline bool hadRuntimeError = false;
 }
 
 inline void report(int line, std::string_view where, std::string_view message)
